b4641bf6a9206737c91c393777c9cdbd0e07279f
Aulia Mahardika <aulia.mahardika@live.com>
2023-04-18 05:07:52 +0700

    update and disabling eslint for build to prod

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,62 +8,15 @@ import { useState } from 'react'
 const inter = Inter({ subsets: ['latin'] })
 
 type AppProps  = {
-	xIsNext: boolean;
-	squares: string[];
-	onSquareClick: () => void;
-	value: number;
-  nextMove: number;
-	onPlay: () => void;
-}
-
-export default function Home() {
-	const [xIsNext, setXIsNext] = useState(true);
-	const [history, setHistory] = useState([Array(9).fill(null)]);
-	const [currentMove, setCurrentMove] = useState(0);
-	const currentSquares = history[currentMove];
-
-  /*
-	function handlePlay(nextSquares) {
-		const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
-		setHistory(nextHistory);
-		setCurrentMove(nextHistory.length - 1);
-
-	}*/
-  
-  function jumpTo({nextMove}: AppProps) {
-    setCurrentMove(nextMove);
-  }
-
-  const moves = history.map((squares, move) => {
-    let description;
-    if (move > 0) {
-      description = 'Go to move #' + move;
-    } else {
-      description = 'Go to game start';
-    }
-    return (
-      <li key={move}>
-        <button onClick={() => console.log("check jumpss")}>{description}</button>
-      </li>
-    );
-  });
-
-	return (
-		<>
-      <div className="game-title">
-		    <h1>Simple Next Tic Tac Toe</h1>
-      </div> 
-      <div className="game">
-        <div className="game-board">
-          <Board xIsNext={xIsNext} squares={currentSquares} onPlay={() => console.log("test button")} />
-        </div>
-        <div className="game-info">
-          <ol>{moves}</ol>
-        </div>
-      </div>
-    </>
-	)
-}
+	xIsNext?: boolean;
+	squares?: string[];
+	onSquareClick?: () => void;
+	value?: string;
+  nextMove?: any;
+	onPlay?: any;
+  nextSquares?: number[];
+  nextHistory?: React.FormEventHandler<HTMLInputElement>;
+};
 
 function Square({value, onSquareClick}: AppProps){
 	return (
@@ -73,19 +26,22 @@ function Square({value, onSquareClick}: AppProps){
 	)
 }
 
-function Board({ xIsNext, squares, onPlay}: AppProps) {
-  function handleClick(i){
-		if (calculateWinner(squares) || squares[i]  ) {
+function Board({ xIsNext, squares, onPlay, onSquareClick}: AppProps) {
+  function handleClick (i: any): any | undefined {
+    //console.log(squares,"check")
+		if (calculateWinner(squares) || squares![i] ) {
 			return;
 		}
-
-	  const nextSquares = squares.slice();
-    
+  
+	  const nextSquares = squares!.slice();
     if (xIsNext) {
       nextSquares[i] = 'X';
     } else {
       nextSquares[i] = 'O';
     }
+    onPlay(nextSquares)
+
+    console.log(squares,"after checks")
   }
   
   const winner = calculateWinner(squares);
@@ -104,26 +60,26 @@ function Board({ xIsNext, squares, onPlay}: AppProps) {
 		<>
 			<div className="status">{status}</div>
 			<div className="board-now">
-				<Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-				<Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-				<Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+				<Square value={squares![0]} onSquareClick={() => handleClick(0)} />
+				<Square value={squares![1]} onSquareClick={() => handleClick(1)} />
+				<Square value={squares![2]} onSquareClick={() => handleClick(2)} />
 			</div>
 			<div className="board-now" >
-				<Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-				<Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-				<Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+				<Square value={squares![3]} onSquareClick={() => handleClick(3)} />
+				<Square value={squares![4]} onSquareClick={() => handleClick(4)} />
+				<Square value={squares![5]} onSquareClick={() => handleClick(5)} />
 			</div>
 			<div className="board-now" >
-				<Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-				<Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-				<Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+				<Square value={squares![6]} onSquareClick={() => handleClick(6)} />
+				<Square value={squares![7]} onSquareClick={() => handleClick(7)} />
+				<Square value={squares![8]} onSquareClick={() => handleClick(8)} />
 			</div>
 		</>
 	)
 }
 
 
-function calculateWinner(squares ) {
+function calculateWinner(squares: any ) {
 
 	const lines = [
 		[0,1,2],
@@ -144,4 +100,55 @@ function calculateWinner(squares ) {
 	}
 
 	return null;
+}
+
+export default function Home() {
+	const [xIsNext, setXIsNext] = useState(true);
+	const [history, setHistory] = useState([Array(9).fill(null)]);
+	const [currentMove, setCurrentMove] = useState(0);
+	const currentSquares = history[currentMove];
+
+  
+	function handlePlay({nextSquares}: AppProps): any {
+		const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+		setHistory(nextHistory as any);
+		setCurrentMove(nextHistory.length - 1);
+
+	}
+  
+  function jumpTo({nextMove, onSquareClick, value, xIsNext}: AppProps): any | undefined {
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((squares: any, move: any) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      
+      <li key={move}>
+        <button onClick={() => console.log("check jumpss")}>{description}</button>
+      </li>
+    );
+  });
+
+	return (
+		<>
+      <div className="game-title">
+		    <h1>Simple Next Tic Tac Toe</h1>
+      </div> 
+      <div className="game">
+        <div className="game-board">
+    {/*<Board xIsNext={xIsNext} squares={currentSquares} onPlay={() => console.log("test button")} /> */}
+          <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+        </div>
+        <div className="game-info">
+          <ol>{moves}</ol>
+        </div>
+      </div>
+    </>
+	)
 }
