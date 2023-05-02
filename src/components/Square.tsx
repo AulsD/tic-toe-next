4d9d3ce2639fc8577aed6a86e8530c03b5d1a234
Aulia Mahardika <aulia.mahardika@live.com>
2023-05-03 00:49:50 +0700

    update grid and create components

---
+++ src/components/Square.tsx
@@ -0,0 +1,25 @@
+type Player = "X" | "O" | "BOTH" | "null";
+
+function Square({
+  winner,
+  value,
+  onClick
+}: {
+  winner: Player;
+  value: Player;
+  onClick: () => void;
+}) {
+  if (!value) {
+    return (
+      <button className="square" onClick={onClick} disabled={Boolean(winner)} />
+    );
+  }
+
+  return (
+    <button className={`square square_${value.toLowerCase()}`} disabled>
+      {value}
+    </button>
+  );
+}
+
+export default Square
